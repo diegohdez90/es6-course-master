cdb65905144221029cb02456aaab8841404a5b59
Diego Arturo Hernandez Fuentes <diegohdez12@gmail.com>
2017-08-08 15:42:52 -0500

    Add exercise 3

---
+++ ex2/ex2-solution.js
@@ -0,0 +1,27 @@
+function foo() { }
+
+function bar() {
+	var a1 = [ 2, 4 ];
+	var a2 = [ 6, 8, 10, 12 ];
+
+	return foo();
+}
+
+console.log(
+	bar().join("") === "281012"
+);
+
+function foo(a,b,c,...rest ) {
+	return [a,...rest]
+}
+
+function bar() {
+	var a1 = [ 2, 4 ];
+	var a2 = [ 6, 8, 10, 12 ];
+
+	return foo(...a1,...a2);	
+}
+
+console.log(
+	bar().join("") === "281012"
+);
